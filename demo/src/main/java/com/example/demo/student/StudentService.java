072411317ce464e1ae01d2b7e164cc0ee0518e47
Divin Irakiza <irakizadivin@gmail.com>
2021-03-10 18:43:19 +0200

    JPA Inititalisor

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,17 +1,19 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L, "Divin", "Irakiza", LocalDate.of(2000, Month.APRIL, 4), 3)
-        );
+        return studentRepository.findAll();
     }
 }
