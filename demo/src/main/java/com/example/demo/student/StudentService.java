7ac1d0d6ffb8a8bb0d3f0de98badd4ce9a27d4b0
Divin Irakiza <irakizadivin@gmail.com>
2021-03-11 19:14:35 +0200

    Student DELETE and PUT APIs

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -3,7 +3,9 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -24,5 +26,26 @@ public class StudentService {
             throw new IllegalStateException("Email already taken");
 
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists)
+            throw new IllegalStateException("Student not found");
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("Student not found"));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent())
+                student.setEmail(email);
+            throw new IllegalStateException("Email exists");
+        }
     }
 }
